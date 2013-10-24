e4484ed1c6c3a91ba97e5b7417cc2e1b4ea1b8b5
Victor Stinner <victor.stinner@enovance.com>
2013-10-24 22:23:28 +0200

    doc2pep: wrap

--- tests/doc2pep.py
+++ tests/doc2pep.py
@@ -5,6 +5,7 @@ into the PEP 454.
 
 import sys
 import re
+import textwrap
 import subprocess
 
 with open("Doc/library/tracemalloc.rst") as fp:
@@ -33,7 +34,26 @@ content = re.sub(
     r"``\1``",
     content)
 
+content = content.splitlines()
+lines = []
+index = 0
+start = None
+while index < len(content):
+    line = content[index]
+    if line.startswith(' '):
+        if start is None:
+            start = index
+    else:
+        if start:
+            pararaph = ' '.join(line.strip() for line in content[start:index])
+            pararaph = textwrap.wrap(pararaph, 72 - 4)
+            for line in pararaph:
+                lines.append('    ' + line)
+        lines.append(content[index])
+        start = None
+    index += 1
+
 with open("pep.rst", "w") as fp:
-    fp.write(content)
+    fp.write('\n'.join(lines))
 
 subprocess.call(["rst2html", "pep.rst", "pep.html"])
